0694e88a460c03702585a335b19d1021d460ec27
Israel Andrade <israel_andrade@Israels-MacBook-Pro.local>
2018-03-04 20:32:02 -0800

    Add all properties for Movie model

--- flix/Models/Movie.swift
+++ flix/Models/Movie.swift
@@ -11,10 +11,21 @@ import Foundation
 class Movie {
     var title: String
     var posterUrl: URL?
-    
+    var backdropUrl: URL?
+    var release_date: String?
+    var overview: String?
+    var backdrop_path: String?
+    var poster_path: String?
+    static let baseURLString = "https://image.tmdb.org/t/p/w500"
     init(dictionary: [String: Any]) {
         title = dictionary["title"] as? String ?? "No title"
-        
+        //let posterPathURL = URL(string: baseURLString + posterPathString)
+        release_date = dictionary["release_date"] as? String ?? "No release date"
+        overview = dictionary["overview"] as? String ?? "No overview"
+        backdrop_path = dictionary["backdrop_path"] as? String ?? "No backdrop path"
+        poster_path = dictionary["poster_path"] as? String ?? "No Poster Path"
         // Set the rest of the properties
+        posterUrl = URL(string: Movie.baseURLString + poster_path!)
+        backdropUrl = URL(string: Movie.baseURLString + backdrop_path!)
     }
 }
